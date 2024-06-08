b0d100ec53ec518a02b2a7c2989ee2dff42b134a
Iriselia <5825014+iriselia@users.noreply.github.com>
2024-06-07 20:47:42 -0700

    add string utils

--- Source/Core/Private/Core/Utilities/StringUtils.h
+++ Source/Core/Private/Core/Utilities/StringUtils.h
@@ -51,10 +51,85 @@ bool Utf8toWStr(const std::string& utf8str, std::wstring& wstr)
 	return true;
 }
 
+bool WStrToUtf8(wchar_t* wstr, size_t size, std::string& utf8str)
+{
+	try
+	{
+		std::string utf8str2;
+		utf8str2.resize(size * 4);                            // allocate for most long case
+
+		if (size)
+		{
+			char* oend = utf8::utf16to8(wstr, wstr + size, &utf8str2[0]);
+			utf8str2.resize(oend - (&utf8str2[0]));               // remove unused tail
+		}
+		utf8str = utf8str2;
+	}
+	catch (std::exception)
+	{
+		utf8str.clear();
+		return false;
+	}
+
+	return true;
+}
+
+bool WStrToUtf8(std::wstring const& wstr, std::string& utf8str)
+{
+	try
+	{
+		std::string utf8str2;
+		utf8str2.resize(wstr.size() * 4);                     // allocate for most long case
+
+		if (wstr.size())
+		{
+			char* oend = utf8::utf16to8(wstr.c_str(), wstr.c_str() + wstr.size(), &utf8str2[0]);
+			utf8str2.resize(oend - (&utf8str2[0]));                // remove unused tail
+		}
+		utf8str = utf8str2;
+	}
+	catch (std::exception)
+	{
+		utf8str.clear();
+		return false;
+	}
+
+	return true;
+}
+
+inline wchar_t wcharToUpper(wchar_t wchar)
+{
+	if (wchar >= L'a' && wchar <= L'z')                      // LATIN SMALL LETTER A - LATIN SMALL LETTER Z
+		return wchar_t(uint16(wchar) - 0x0020);
+	if (wchar == 0x00DF)                                     // LATIN SMALL LETTER SHARP S
+		return wchar_t(0x1E9E);
+	if (wchar >= 0x00E0 && wchar <= 0x00F6)                  // LATIN SMALL LETTER A WITH GRAVE - LATIN SMALL LETTER O WITH DIAERESIS
+		return wchar_t(uint16(wchar) - 0x0020);
+	if (wchar >= 0x00F8 && wchar <= 0x00FE)                  // LATIN SMALL LETTER O WITH STROKE - LATIN SMALL LETTER THORN
+		return wchar_t(uint16(wchar) - 0x0020);
+	if (wchar >= 0x0101 && wchar <= 0x012F)                  // LATIN SMALL LETTER A WITH MACRON - LATIN SMALL LETTER I WITH OGONEK (only %2=1)
+	{
+		if (wchar % 2 == 1)
+			return wchar_t(uint16(wchar) - 0x0001);
+	}
+	if (wchar >= 0x0430 && wchar <= 0x044F)                  // CYRILLIC SMALL LETTER A - CYRILLIC SMALL LETTER YA
+		return wchar_t(uint16(wchar) - 0x0020);
+	if (wchar == 0x0451)                                     // CYRILLIC SMALL LETTER IO
+		return wchar_t(0x0401);
+
+	return wchar;
+}
+
+inline wchar_t wcharToUpperOnlyLatin(wchar_t wchar)
+{
+	return isBasicLatinCharacter(wchar) ? wcharToUpper(wchar) : wchar;
+}
+
+
 bool Utf8ToUpperOnlyLatin(std::string& utf8String)
 {
 	std::wstring wstr;
-	if (!Utf8toWStr(utf8String, wstr))
+	if (!Utf8toWStr(utf8String, ,wstr))
 		return false;
 
 	std::transform(wstr.begin(), wstr.end(), wstr.begin(), wcharToUpperOnlyLatin);
