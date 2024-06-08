9fb84b18d83460b4bec28c4e315a24af66b3689e
Iriselia <5825014+iriselia@users.noreply.github.com>
2024-06-07 20:47:42 -0700

    update spdlog

--- Source/Core/Public/Detail/Logger.h
+++ Source/Core/Public/Detail/Logger.h
@@ -5,43 +5,44 @@ class Logger
 {
 	DECLARE_SINGLETON(Logger);
 private:
-	std::shared_ptr<spdlog::logger> Console;
+	std::shared_ptr<spdlog::logger> LoggerImpl;
 public:
 	void Init()
 	{
-		//Console = spdlog::stdout_logger_mt("console", true);
-		Console = spdlog::get("Console");
-	}
+		//LoggerImpl = spdlog::stdout_logger_mt("console", true);
+		//LoggerImpl = spdlog::get("LoggerImpl");
+		LoggerImpl = spdlog::stdout_color_mt("Logger");
 
+	}
 
 	template <typename... Args>
 	inline void Critical(const char* fmt, const Args&... args)
 	{
-		Console->log(spdlog::level::critical, fmt, args...);
+		LoggerImpl->log(spdlog::level::critical, fmt, args...);
 	}
 
 	void Test()
 	{
-		Console->info("Welcome to spdlog!");
-		Console->error("An info message example {}..", 1);
+		LoggerImpl->info("Welcome to spdlog!");
+		LoggerImpl->error("An info message example {}..", 1);
 
 		// Formatting examples
-		Console->warn("Easy padding in numbers like {:08d}", 12);
-		Console->critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
-		Console->info("Support for floats {:03.2f}", 1.23456);
-		Console->info("Positional args are {1} {0}..", "too", "supported");
+		LoggerImpl->warn("Easy padding in numbers like {:08d}", 12);
+		LoggerImpl->critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+		LoggerImpl->info("Support for floats {:03.2f}", 1.23456);
+		LoggerImpl->info("Positional args are {1} {0}..", "too", "supported");
 
-		Console->info("{:<30}", "left aligned");
-		Console->info("{:>30}", "right aligned");
-		Console->info("{:^30}", "centered");
+		LoggerImpl->info("{:<30}", "left aligned");
+		LoggerImpl->info("{:>30}", "right aligned");
+		LoggerImpl->info("{:^30}", "centered");
 
 		spdlog::get("console")->info("loggers can be retrieved from a global registry using the spdlog::get(logger_name) function");
 
 		// Runtime log levels
 		spdlog::set_level(spdlog::level::info); //Set global log level to info
-		Console->debug("This message shold not be displayed!");
-		Console->set_level(spdlog::level::debug); // Set specific logger's log level
-		Console->debug("This message shold be displayed..");
+		LoggerImpl->debug("This message shold not be displayed!");
+		LoggerImpl->set_level(spdlog::level::debug); // Set specific logger's log level
+		LoggerImpl->debug("This message shold be displayed..");
 
 		// Create basic file logger (not rotated)
 		auto my_logger = spdlog::basic_logger_mt("basic_logger", "basic.txt");
@@ -63,8 +64,8 @@ public:
 
 		// Compile time debug or trace macros.
 		// Enabled #ifdef SPDLOG_DEBUG_ON or #ifdef SPDLOG_TRACE_ON
-		SPDLOG_TRACE(Console, "Enabled only #ifdef SPDLOG_TRACE_ON..{} ,{}", 1, 3.23);
-		SPDLOG_DEBUG(Console, "Enabled only #ifdef SPDLOG_DEBUG_ON.. {} ,{}", 1, 3.23);
+		SPDLOG_TRACE(LoggerImpl, "Enabled only #ifdef SPDLOG_TRACE_ON..{} ,{}", 1, 3.23);
+		SPDLOG_DEBUG(LoggerImpl, "Enabled only #ifdef SPDLOG_DEBUG_ON.. {} ,{}", 1, 3.23);
 	}
 };
 
