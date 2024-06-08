eb3b4eccf22c6dce5ff255cfdd8e05cc1c3400a3
Iriselia <5825014+iriselia@users.noreply.github.com>
2024-06-07 20:46:21 -0700

    refactor

--- Source/Core/Interface/Core.pch.h
+++ Source/Core/Interface/Core.pch.h
@@ -3,10 +3,6 @@
 /*----------------------------------------------------------------------------
 Low level includes.
 ----------------------------------------------------------------------------*/
-#include "Private/Core/LanguageFeatures.h"			/// 1 C/C++ standard libraries
+#include "Private/Core/StandardLanguageFeatures.h"	/// 1 C/C++ standard libraries
 #include "Private/Core/PlatformAbstractionLayer.h"	/// 2 Windows, MacOS, or Linux
-#include "Private/Core/Utilities.h"					/// 3 Custom C++ extensions
-
-#include "Public/Detail/Config.h"
-#include "Public/Detail/Logger.h"
-#include "Public/Detail/FileSystem.h"
+#include "Private/Core/CustomLanguageExtensions.h"	/// 3 Custom C++ extensions
--- Source/Core/Interface/Core.pub.h
+++ Source/Core/Interface/Core.pub.h
@@ -5,8 +5,8 @@ Low level includes.
 ----------------------------------------------------------------------------*/
 #include "Public/Core.h"
 
-#include "Public/Detail/Config.h"
-#include "Public/Detail/Logger.h"
+// #include "Public/Detail/Config.h"
+// #include "Public/Detail/Logger.h"
 // #include "Public/ChatMessage.h"
 // #include "Public/Platform.h"
 // #include "Public/ProducerConsumerQueue.h"
